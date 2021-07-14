d5a770d1ba97fcf8405f8ea366737fc1ef5f7e19
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-14 18:48:25 -0300

    Added content model with a generic relation to associate any kind of content

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -37,3 +38,10 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_obj = GenericForeignKey('content_type', 'object_id')
