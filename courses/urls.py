7703d05d5593bd117e40930f4d4a6dc3d9a5fb43
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-15 16:22:02 -0300

    Manager modules and contents

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView
+from .views import ManageCourseListView, CourseCreateView, CourseUpdateView,\
+    CourseDeleteView, CourseModuleUpdateView, ContentCreateUpdateView, ContentDeleteView, \
+    ModuleContentListView
 
 app_name = 'courses'
 
@@ -8,4 +10,12 @@ urlpatterns = [
     path('create/', CourseCreateView.as_view(), name='create'),
     path('<pk>/edit/', CourseUpdateView.as_view(), name='edit'),
     path('<pk>/delete/', CourseDeleteView.as_view(), name='delete'),
+    path('<pk>/module/', CourseModuleUpdateView.as_view(), name='course_module_update'),
+
+    path('module/<int:module_id>/content/<model_name>/create/', ContentCreateUpdateView.as_view(),
+         name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<int:id>/', ContentCreateUpdateView.as_view(),
+         name='module_content_update'),
+    path('content/<int:id>/delete/', ContentDeleteView.as_view(), name='module_content_delete'),
+    path('module/<int:module_id>/', ModuleContentListView.as_view(), name='module_content_list'),
 ]
