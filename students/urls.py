2dede600a8ba285f29ebcd07349788d3b72bf105
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-18 14:13:15 -0300

    Created a student registration view

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import StudentRegistrationView
+
+app_name = 'students'
+
+urlpatterns = [
+    path('register/', StudentRegistrationView.as_view(), name='registration'),
+]
