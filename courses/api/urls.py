a9091050865e10c8685f140a3750d54d73604de1
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-19 17:17:06 -0300

    Created Course viewsets and routers

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
-from .views import SubjectListView, SubjectDetailView, CourseEnrollView
+from django.urls import path, include
+from .views import SubjectListView, SubjectDetailView, CourseEnrollView, CourseViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', CourseViewSet)
+
 
 app_name = 'courses'
 
@@ -7,4 +12,5 @@ urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
     path('courses/<int:pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
+    path('', include(router.urls)),
 ]
