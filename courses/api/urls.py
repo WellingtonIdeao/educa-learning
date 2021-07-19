7c55bbdf6f754af4e763d9e383fa36004f85107d
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-19 17:23:37 -0300

    Added additional action enroll to CourseViewSet

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -11,6 +11,5 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
-    path('courses/<int:pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
     path('', include(router.urls)),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
 
@@ -32,3 +33,12 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True,
+            method=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
