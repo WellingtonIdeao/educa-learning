fd90cda96a5f5585d4ea40ad73b374cd2941b84a
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-19 16:00:46 -0300

    Build a custom Course API view with authentication and permission

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SubjectListView, SubjectDetailView
+from .views import SubjectListView, SubjectDetailView, CourseEnrollView
 
 app_name = 'courses'
 
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
+    path('courses/<int:pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
 ]
